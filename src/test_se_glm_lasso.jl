9b4c8b6e804d3e80e296e7ccaf8b19c90c7f6411
Aleksandr Aravkin <sasha.aravkin@gmail.com>
2017-01-18 14:34:31 -0800

    added inaccurate cross-validation solving (careful with this...) added profiling

--- src/test_se_glm_lasso.jl
+++ src/test_se_glm_lasso.jl
@@ -6,7 +6,7 @@ include("kfold_cv.jl")
 
 
 # we are testing the standard error of the sample mean for iid N(0,1)
-phi = 0.0
+phi = 0.3
 mu = 0
 sd = 1
 
@@ -14,7 +14,7 @@ sd = 1
 
 
 # method 1: using Monte Carlo
-nsim = 500
+nsim = 50
 ndata = 100
 
 
@@ -29,12 +29,21 @@ sd_mc = std(means)
 
 # method 2: using our method
 
+profile = true
+
 se_glm = zeros(nsim)
-for i in 1:nsim
+
+if profile
+   Profile.clear()
+
+    @profile for i in 1:nsim
     data_raw = simulate_AR1(ndata, phi)
     data = mu_IF(data_raw)
     se_glm[i] = se_glm_lasso(data)
+   end
 end
+
+Profile.print(format = :flat)
 
 println("The theoretical StdDev is $(sd/sqrt(ndata))")
 println("The standard error using Monte Carlo is $sd_mc")
